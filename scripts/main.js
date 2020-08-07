f73f9eb21ad64604ffb31e4b033336657ea9eaa5
Roman Shmelev <r-shmelev@mail.ru>
2020-08-07 18:39:23 +0300

    lesson02

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,18 @@
-let money = 100;
-let income = 100000;
-let addExpenses = 150;
-let deposit = 1000;
+let money = 150000;
+let income = 'work';
+let addExpenses = 'taxi, internet, phone';
+let deposit = true;
 let mission = 1000000;
-let period = 1;
+let period = 6;
+let budgetDay = 50000;
 
-alert("hello world");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(period + ' месяцев ' + mission);
 
-console.log("200");
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(' '));
+
+console.log(Math.floor (budgetDay / 30));
